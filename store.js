7db8321b959463fb4d177c64c3266cf1c0823ef3
fackLufei <18058640972@163.com>
2020-03-20 23:29:49 +0800

    996

--- store.js
+++ store.js
@@ -6,18 +6,22 @@ Vue.use(Vuex);
 
 export default  new Vuex.Store({  
     state: {  
-        userInfo:getUserInfo(), 
-        token: getToken()||'',  
-        classifyList:[]//首页分类数据
+        userInfo:getUserInfo(), //用户信息
+        token: getToken()||'',  //token
+        classifyList:[],//首页分类数据
+		routerWait:{}//{routerPath:'',routerData:{}}
     },  
     mutations: {    
-        logout(state) { 
+        logout(state) {//退出登录
 			 state.userInfo = {};
 			 state.token = '';
              uni.clearStorageSync();
         },
-		setClassifyList(state, data){
+		setClassifyList(state, data){//商品分类一级数据
 			state.classifyList = data;
+		},
+		setRouterWait(state, data){//跳转拦截数据
+			state.routerWait = data;
 		}
     }  
 })  
