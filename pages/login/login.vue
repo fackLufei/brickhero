673af5f14f5b42db385506b424c2c5c91251ddc5
fackLufei <18058640972@163.com>
2020-03-20 23:27:12 +0800

    996

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -9,66 +9,84 @@
 			</view>
 		</view>
 		<view class="login-in">
-			<van-button @getuserinfo="wxLogin" color="linear-gradient(to right, #45E26A, #37CB67)" open-type="getUserInfo" type="primary" lang="zh_CN"  block>微信用户一键登录</van-button>
+			<van-button @getuserinfo="wxLogin" color="linear-gradient(to right, #45E26A, #37CB67)" open-type="getUserInfo" type="primary"
+			 lang="zh_CN" block>微信用户一键登录</van-button>
 		</view>
 		<view class="phone-login">
-			<text  @click="phoneLogin">账号密码登录</text>
-			<text  @click="register">注册</text>
-			</view>
+			<text @click="phoneLogin">账号密码登录</text>
+			<text @click="register">注册</text>
+		</view>
 		<view class="tips">登录即代表您已经同意搬砖侠<text>《服务协议》</text>与<text>《隐私政策》</text></view>
 	</view>
 </template>
 
 <script>
-	import {wxLogin} from '@/api/login.js';
-	import {setToken,setUserInfo} from '@/utils/auth.js';
+	import {
+		wxLogin
+	} from '@/api/login.js';
+	import {
+		setToken,
+		setUserInfo
+	} from '@/utils/auth.js';
+	import {
+		goPage
+	} from '@/utils/index.js';
+	import {
+		mapState
+	} from 'vuex';
 	export default {
-		data() {
-			return {
-
-			};
+		computed: {
+			...mapState(['routerWait'])
 		},
 		methods: {
-			register(){
+			register() {
 				uni.navigateTo({
-					url:'/pages/register/register'
+					url: '/pages/register/register'
 				});
 			},
-			phoneLogin(){
+			phoneLogin() {
 				uni.navigateTo({
-					url:'/pages/phoneLogin/phoneLogin'
+					url: '/pages/phoneLogin/phoneLogin'
 				})
 			},
 			wxLogin(e) {
-				
+
 				uni.login({
 					provider: 'weixin',
 					success: function(loginRes) {
 						console.log({
-							code:loginRes.code,
-							encryptedData:e.detail.encryptedData,
-							errMsg:e.detail.errMsg,
-							iv:e.detail.iv,
-							rawData:e.detail.rawData,
-							signature:e.detail.signature,
-							userInfo:e.detail.userInfo
+							code: loginRes.code,
+							encryptedData: e.detail.encryptedData,
+							errMsg: e.detail.errMsg,
+							iv: e.detail.iv,
+							rawData: e.detail.rawData,
+							signature: e.detail.signature,
+							userInfo: e.detail.userInfo
 						});
 						// 微信登录
 						wxLogin({
-							code:loginRes.code,
-							encryptedData:e.detail.encryptedData,
-							errMsg:e.detail.errMsg,
-							iv:e.detail.iv,
-							rawData:e.detail.rawData,
-							signature:e.detail.signature,
-							userInfo:e.detail.userInfo
-						}).then(res=>{
-							if(res.code === 200){
+							code: loginRes.code,
+							encryptedData: e.detail.encryptedData,
+							errMsg: e.detail.errMsg,
+							iv: e.detail.iv,
+							rawData: e.detail.rawData,
+							signature: e.detail.signature,
+							userInfo: e.detail.userInfo
+						}).then(res => {
+							if (res.code === 200) {
 								setToken(res.data.token);
 								setUserInfo(res.data.userInfo);
+								if (this.routerWait.routerPath) {
+									goPage(this.routerWait.routerType, this.routerWait.routerPath);
+								}
 								uni.showToast({
-									icon:'none',
-									title:'登录成功'
+									icon: 'none',
+									title: '登录成功'
+								})
+							} else {
+								uni.showToast({
+									icon: 'none',
+									title: res.message
 								})
 							}
 						})
