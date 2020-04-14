6ba4424bb6dc448d518cc713643c7cbabd895812
fackLufei <18058640972@163.com>
2020-04-14 20:04:20 +0800

    996

--- pages/productDetails/productDetails.vue
+++ pages/productDetails/productDetails.vue
@@ -8,6 +8,22 @@
 		</swiper>
 		<!-- 商品名字 -->
 		<view class="product-name">{{goods.name}}</view>
+		<!-- info -->
+		<view class="goods-info">
+			<view class="c">
+				<text class="desc">{{goods.brief}}</text>
+				<view class="price">
+					<view class="counterPrice">原价：￥{{goods.counterPrice}}</view>
+					<view class="retailPrice">现价：￥{{goods.retailPrice}}</view>
+				</view>
+
+				<view class="brand" v-if="brand.name">
+					<navigator :url="'../brandDetails/brandDetails?id='+brand.id">
+						<text>{{brand.name}}</text>
+					</navigator>
+				</view>
+			</view>
+		</view>
 		<!-- html 详情 -->
 		<jyf-parser v-if="goods.detail" :html="goods.detail" ref="article" style="font-size: 0;"></jyf-parser>
 	</view>
@@ -27,7 +43,8 @@
 		},
 		data() {
 			return {
-				goods:{}
+				goods: {}, //商品详情
+				brand: {} //品牌
 			}
 		},
 		onLoad(option) {
@@ -35,19 +52,20 @@
 			getProductDetail(_currProductId).then(res => {
 				if (res.errno === 0) {
 					console.log(res.data);
-					this.goods = res.data.info
+					this.goods = res.data.info;
+					this.brand = res.data.brand;
 				}
 			})
 		},
 		// 页面分享L
 		onShareAppMessage: function() {
-			
+
 			return {
 				title: '我是你亲爱的小黑',
 				desc: '唯爱与美食不可辜负',
 				path: '/pages/index/index?productId=' + _currProductId
 			}
-			
+
 		},
 		methods: {
 
@@ -56,32 +74,101 @@
 </script>
 
 <style lang="scss">
-	page{
+	page {
 		background-color: #f4f4f4;
 	}
+
 	.product-details {
 		.info-details {
 			font-size: 0;
 		}
 
 		.goodsimgs {
-			width: 750rpx;
-			height: 750rpx;
+			width: 750upx;
+			height: 750upx;
 		}
 
 		.goodsimgs image {
-			width: 750rpx;
-			height: 750rpx;
+			width: 750upx;
+			height: 750upx;
 		}
-		
-		.product-name{
+
+		.product-name {
 			height: 86upx;
 			font-weight: 550;
 			line-height: 86upx;
 			margin-left: 35upx;
 			font-size: 38upx;
 			letter-spacing: 1upx;
+			font-size: 29upx;
+			color: #333;
+		}
 
+		.goods-info {
+			overflow: hidden;
+			background: #fff;
+		}
+
+		.goods-info .c {
+			display: block;
+			height: 100%;
+			margin-left: 31.25upx;
+			padding: 38upx 31.25upx 38upx 0;
+			border-bottom: 1px solid #f4f4f4;
+		}
+
+		.goods-info .c text {
+			display: block;
+			width: 687.5upx;
+			text-align: left;
+		}
+
+		.goods-info .desc {
+			height: 43upx;
+			margin-bottom: 41upx;
+			font-size: 24upx;
+			line-height: 36upx;
+			color: #999;
+		}
+
+		.goods-info .price {
+			display: flex;
+		}
+
+		.goods-info .counterPrice {
+			padding-left: 0upx;
+			text-decoration: line-through;
+			font-size: 30upx;
+			color: #999;
+		}
+
+		.goods-info .retailPrice {
+			padding-left: 5%;
+			font-size: 30upx;
+			color: #a78845;
+		}
+
+		.goods-info .brand {
+			margin-top: 23upx;
+			min-height: 40upx;
+			text-align: left;
+		}
+
+		.goods-info .brand {
+			margin-top: 23upx;
+			min-height: 40upx;
+			text-align: left;
+		}
+
+		.goods-info .brand text {
+			display: inline-block;
+			width: auto;
+			padding: 2px 30upx 2px 10.5upx;
+			line-height: 35.5upx;
+			border: 1px solid #f48f18;
+			font-size: 25upx;
+			color: #f48f18;
+			border-radius: 4upx;
 		}
 	}
 </style>
